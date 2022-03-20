8157441fde086e18a4652123b280305c72035d4a
abubjazov <abubjazov@mail.ru>
2022-03-20 15:47:48 +0300

    components/: +Checkout

--- src/components/Checkout/Checkout.tsx
+++ src/components/Checkout/Checkout.tsx
@@ -3,7 +3,7 @@ import './Checkout.scss'
 export const Checkout = () => (
 	<section className='checkout'>
 		<h2 className='total'>
-			Total: <span>177.16</span>$
+			Total: <span>177.16</span> $
 		</h2>
 		<button>Checkout</button>
 	</section>
