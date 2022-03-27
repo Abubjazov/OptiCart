108a43147f5a61d7f8f81b5109efd6cf5232f923
abubjazov <abubjazov@mail.ru>
2022-03-27 15:31:43 +0300

    MainPage: refactor by Redux/Thunk

---
+++ src/store/action-creators/product.actionCreators.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import { Dispatch } from 'redux'
+
+import { ProductAction, ProductActionTypes } from '../../interfaces'
+
+export const fetchProducts = () => {
+	return async (dispatch: Dispatch<ProductAction>) => {
+		try {
+			dispatch({ type: ProductActionTypes.FETCH_PRODUCTS })
+
+			const response = await axios.get(
+				process.env.REACT_APP_BASE_URL + 'products'
+			)
+
+			dispatch({
+				type: ProductActionTypes.FETCH_PRODUCTS_SUCCESS,
+				payload: response.data.products,
+			})
+		} catch (error) {
+			dispatch({
+				type: ProductActionTypes.FETCH_PRODUCTS_ERROR,
+				payload: `An error occurred while loading the products! ${error}`,
+			})
+		}
+	}
+}
