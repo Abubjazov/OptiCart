5222c420533c6bd7eccb39764306fcae931b33e8
abubjazov <abubjazov@mail.ru>
2022-03-28 20:56:08 +0300

    utils: +utils.test: fullPrice(), getTotal()

---
+++ src/mockData/mockData.ts
@@ -0,0 +1,31 @@
+import { CartListItem } from '../interfaces'
+
+export const cartItems: CartListItem[] = [
+	{
+		id: 2,
+		product_id: 15,
+		name: 'Rangoto Vi',
+		picture: 'https://i.ibb.co/T2XSnw0/z9.webp',
+		description: 'Nemo dolore officia est natus.',
+		price: 11.04,
+		quantity: 3,
+	},
+	{
+		id: 3,
+		product_id: 16,
+		name: 'Armo Sento 2',
+		picture: 'https://i.ibb.co/TPT93hh/z10.jpg',
+		description: 'Nobis in voluptatum qui voluptate.',
+		price: 17.22,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		product_id: 9,
+		name: 'Vertigo 2',
+		picture: 'https://i.ibb.co/vvCZ6gp/nike.webp',
+		description: 'Architecto omnis sed nostrum aliquid.',
+		price: 26.11,
+		quantity: 2,
+	},
+]
