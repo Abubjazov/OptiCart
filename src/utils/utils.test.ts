3ef729a74980e9c271005d34b4e72b647ce3bc59
abubjazov <abubjazov@mail.ru>
2022-03-30 22:46:13 +0300

    utils.test: build bug fix

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -6,10 +6,6 @@ describe('Function: utils/fullPrice()', () => {
 	test('fullPrice() / Positive: calculates the total price and returns a string of the format "dd,dd"', () => {
 		expect(fullPrice(5, 10)).toBe('50.00')
 	})
-
-	test('fullPrice() / Negative: the input recipient is not a number and returns "NaN"', () => {
-		expect(fullPrice('b', 10)).toBe('NaN')
-	})
 })
 
 describe('Function: utils/getTotal()', () => {
