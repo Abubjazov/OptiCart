45bb4785e88c1143a4192aebcfe2971f8a3ca0c1
abubjazov <abubjazov@mail.ru>
2022-04-04 21:58:30 +0300

    improved performance: App: +lazy/Suspense, disabled unused JS

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,17 +1,28 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { Header } from '../components'
-import { CartPage, MainPage, Page404 } from '../pages'
+import { Header, Spinner } from '../components'
+import { MainPage } from '../pages'
+
+const CartPage = lazy(() =>
+	import('../pages').then(({ CartPage }) => ({ default: CartPage }))
+)
+
+const Page404 = lazy(() =>
+	import('../pages').then(({ Page404 }) => ({ default: Page404 }))
+)
 
 export const App = (): JSX.Element => {
 	return (
 		<BrowserRouter>
 			<Header />
-			<Routes>
-				<Route path='/optiCart/' element={<MainPage />} />
-				<Route path='/optiCart/cart' element={<CartPage />} />
-				<Route path='*' element={<Page404 />} />
-			</Routes>
+			<Suspense fallback={<Spinner />}>
+				<Routes>
+					<Route path='/optiCart/' element={<MainPage />} />
+					<Route path='/optiCart/cart' element={<CartPage />} />
+					<Route path='*' element={<Page404 />} />
+				</Routes>
+			</Suspense>
 		</BrowserRouter>
 	)
 }
