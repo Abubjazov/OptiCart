946658f1247d9180e73dcc69e66a9491d9c97ae2
abubjazov <abubjazov@mail.ru>
2022-04-06 14:33:48 +0300

    ErrorMessage: refactor

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -4,7 +4,8 @@ import './ErrorMessage.scss'
 
 export const ErrorMessage = ({ error }: ErrorMessageProps): JSX.Element => (
 	<div className='error-message'>
-		<p>{error}</p>
+		<p>{error?.split('*')[0]}</p>
+		<p>{error?.split('*')[1]}</p>
 
 		<NavLink end to='/optiCart/'>
 			Back to main page
--- src/store/actionCreators/cart.actionCreators.ts
+++ src/store/actionCreators/cart.actionCreators.ts
@@ -19,7 +19,7 @@ export const fetchCart = () => {
 		} catch (error) {
 			dispatch({
 				type: CartActionTypes.FETCH_CART_ERROR,
-				payload: `An error occurred while loading the cart!\n${error}`,
+				payload: `An error occurred while loading the cart!*${error}`,
 			})
 		}
 	}
@@ -42,7 +42,7 @@ export const addToCart = (product_id: number) => {
 		} catch (error) {
 			dispatch({
 				type: CartActionTypes.ADD_TO_CART_ERROR,
-				payload: `An error occurred while adding an item to the cart!\n${error}`,
+				payload: `An error occurred while adding an item to the cart!*${error}`,
 			})
 		}
 	}
@@ -64,7 +64,7 @@ export const removeFromCart = (product_id: number) => {
 		} catch (error) {
 			dispatch({
 				type: CartActionTypes.REMOVE_FROM_CART_ERROR,
-				payload: `An error occurred while removing an item from the cart!\n${error}`,
+				payload: `An error occurred while removing an item from the cart!*${error}`,
 			})
 		}
 	}
@@ -90,7 +90,7 @@ export const updateCartQuantity = (cartItemId: number, quantity: number) => {
 		} catch (error) {
 			dispatch({
 				type: CartActionTypes.UPDATE_CART_QUANTITY_ERROR,
-				payload: `An error occurred while updating an cart item quantity!\n${error}`,
+				payload: `An error occurred while updating an cart item quantity!*${error}`,
 			})
 		}
 	}
@@ -109,7 +109,7 @@ export const checkoutCart = () => {
 		} catch (error) {
 			dispatch({
 				type: CartActionTypes.CHECKOUT_CART_ERROR,
-				payload: `An error occurred while checkout!\n${error}`,
+				payload: `An error occurred while checkout!*${error}`,
 			})
 		}
 	}
